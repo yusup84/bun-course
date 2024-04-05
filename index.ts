165fb6c411229f1f9579b01d7cf5c6e31eccd561
yusup84 <globalinformatika84@gmail.com>
2024-04-05 15:00:38 +0700

    04-streaming-file

--- index.ts
+++ index.ts
@@ -3,21 +3,24 @@ import figlet from "figlet";
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    const url = new URL(req.url)
-    if(url.pathname === '/'){
-        const body = figlet.textSync('hola')
-        return new Response(body)
+    const url = new URL(req.url);
+    if (url.pathname === "/") {
+      const body = figlet.textSync("hola");
+      return new Response(body);
     }
-    if(url.pathname === '/about'){
-        return new Response('About Me')
+    if (url.pathname === "/about") {
+      return new Response("About Me");
     }
-    if(url.pathname === '/contact'){
-        return new Response('Contact Us!')
+    if (url.pathname === "/contact") {
+      return new Response("Contact Us!");
     }
-    if(url.pathname === '/feed'){
-        throw new Error('Could not fetch feed')
+    if (url.pathname === "/feed") {
+      throw new Error("Could not fetch feed");
     }
-    return new Response('404!')
+    if (url.pathname === "/greet") {
+      return new Response(Bun.file('./greet.txt'));
+    }
+    return new Response("404!");
   },
 });
 
